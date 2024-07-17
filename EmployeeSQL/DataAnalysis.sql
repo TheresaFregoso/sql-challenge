e89c369a9101d91579609933a78fffa2d7325f10
Theresa Fregoso <166779703+TheresaFregoso@users.noreply.github.com>
2024-07-16 20:12:07 -0700

    Final edits

--- EmployeeSQL/DataAnalysis.sql
+++ EmployeeSQL/DataAnalysis.sql
@@ -39,8 +39,59 @@ FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 --List the manager of each department along with their department number, department name, employee number, last name, and first name.
+CREATE VIEW manager_info AS
+SELECT dm.dept_no,
+	dm.emp_no,
+	d.dept_name,
+	e.last_name,
+	e.first_name
+FROM "Dept_manager" as dm
+INNER JOIN "Departments" as d on
+dm.dept_no = d.dept_no
+INNER JOIN "employees" as e ON
+dm.emp_no = e.emp_no;
+
+SELECT *
+FROM manager_info;
+
 --List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+create view	dept_emp_info AS
+Select	de.depth_no,
+	de.emp_no, 
+	e.last_name,
+	e.first_name,
+	d.dept_name
+FROM "Dept_emp" as de
+INNER JOIN "employees" AS e
+ON e.emp_no = de.emp_no
+INNER JOIN "Departments" AS d
+ON d.dept_no = de.depth_no;
+
+SELECT *
+FROM dept_emp_info;
 --List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT e.first_name,
+	e.last_name,
+	e.sex
+FROM employees e
+WHERE first_name = 'Hercules' and last_name LIKE 'B%';
+
 --List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT dei.emp_no,
+	dei.last_name,
+	dei.first_name
+FROM dept_emp_info as dei where dept_name = 'Sales';
+
 --List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT dei.emp_no,
+	dei.last_name,
+	dei.first_name,
+	dei.dept_name
+FROM dept_emp_info as dei where dept_name = 'Sales' or dept_name = 'Development';
+
 --List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT e.last_name,
+	COUNT(e.last_name) as frequency
+FROM employees as e
+GROUP BY e.last_name
+ORDER BY frequency DESC;
--- EmployeeSQL/data/QuickDBD-EmployeeDB.sql
+++ EmployeeSQL/data/QuickDBD-EmployeeDB.sql
@@ -3,27 +3,6 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Departments" (
-    "dept_no" VARCHAR(10)   NOT NULL,
-    "dept_name" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "dept_no"
-     )
-);
-
-CREATE TABLE "Dept_emp" (
-    "emp_no" INT   NOT NULL,
-    "depth_no" VARCHAR(10)   NOT NULL
-);
-
-CREATE TABLE "titles" (
-    "title_id" VARCHAR(10)   NOT NULL,
-    "title" VARCHAR(25)   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
-);
-
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
     "emp_title_id" VARCHAR(10)   NOT NULL,
@@ -37,18 +16,45 @@ CREATE TABLE "employees" (
      )
 );
 
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
 CREATE TABLE "Dept_manager" (
     "dept_no" VARCHAR(10)   NOT NULL,
-    "emp_no" INT   NOT NULL
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "Dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "depth_no" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
+        "emp_no","depth_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(25)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
 );
 
 CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL
+    "salary" INT   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
-
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_depth_no" FOREIGN KEY("depth_no")
-REFERENCES "Departments" ("dept_no");
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
@@ -58,6 +64,12 @@ REFERENCES "Departments" ("dept_no");
 
 ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_depth_no" FOREIGN KEY("depth_no")
+REFERENCES "Departments" ("dept_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
